fdd68569415bef452b8cdf8d2fc392df71ec10cb
James Dawson <james.dawson@endjin.com>
2021-03-22 14:48:41 +0000

    Updated tests for 'Add-Path' and 'Export-Variable'

--- module/functions/Add-Path.Tests.ps1
+++ module/functions/Add-Path.Tests.ps1
@@ -3,11 +3,14 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe 'Add-Path Tests' {
+
+    $env:GITHUB_PATH = "TestDrive:/github_path.txt"
     
-    Mock Write-Output { } -Verifiable -ParameterFilter { $InputObject -eq "`n::add-path::C:\foo\bar" }
+    #Mock Write-Information { } -Verifiable -ParameterFilter { $InputObject -eq "`n::add-path::C:\foo\bar" }
     Add-Path -Path "C:\foo\bar"
 
-    It 'should output the correct message' {
-        Assert-VerifiableMock
+    It 'should add the path to the correct file' {
+        $env:GITHUB_PATH | Should -Exist
+        "C:\foo\bar" | Should -BeIn (Get-Content $env:GITHUB_PATH)
     }
 }
--- module/functions/Export-Variable.Tests.ps1
+++ module/functions/Export-Variable.Tests.ps1
@@ -4,10 +4,15 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
 Describe 'Export-Variable Tests' {
     
-    Mock Write-Output { } -Verifiable -ParameterFilter { $InputObject -eq "`n::set-env name=foo::bar" }
+    $env:GITHUB_ENV = "TestDrive:/github_env.txt"
+
     Export-Variable -Name foo -Value bar
 
-    It 'should output the correct message' {
-        Assert-VerifiableMock
+    It 'should add the variable mapping to the correct file' {
+        $key,$value = (Get-Content $env:GITHUB_ENV) -split "="
+        
+        $env:GITHUB_ENV | Should -Exist
+        $key | Should -Be "foo"
+        $value | Should -Be "bar"
     }
 }
